70bed57334a5e1a1a753d9eaa46fafcec5aba22b
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-03-27 10:38:49 +0530

    update

--- src/main/java/spring/core/model/Employee.java
+++ src/main/java/spring/core/model/Employee.java
@@ -1,9 +1,5 @@
 package spring.core.model;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
@@ -20,21 +16,14 @@ public class Employee {
 	private int id;
 	private String name,gender;
 	private Address address;
-	private List<Integer> list;
-	private Set<Integer> set;
-	private Map<Integer, String> map;
 	
-	
-	public Employee(int id, String name, String gender, Address address, List<Integer> list, Set<Integer> set,
-			Map<Integer, String> map) {
+	public Employee(int id, String name, String gender, Address address) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.gender = gender;
 		this.address = address;
-		this.list = list;
-		this.set = set;
-		this.map = map;
+		System.out.println("Employee.Employee()");
 	}
 
 	public void setId(int id) {
@@ -53,18 +42,6 @@ public class Employee {
 	public void setAddress(Address address) {
 		this.address = address;
 		System.out.println("Employee.setAddress()");
-	}
-
-	public void setList(List<Integer> list) {
-		this.list = list;
-	}
-
-	public void setSet(Set<Integer> set) {
-		this.set = set;
-	}
-
-	public void setMap(Map<Integer, String> map) {
-		this.map = map;
 	}
 	
 }
