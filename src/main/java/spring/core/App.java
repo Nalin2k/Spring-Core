4bc9b38376e1d9a97ccffb05b0109449b32fbdf8
Technical Savage <168007796+Nalin2k@users.noreply.github.com>
2025-03-27 13:05:04 +0530

    update

--- src/main/java/spring/core/App.java
+++ src/main/java/spring/core/App.java
@@ -9,10 +9,10 @@ public class App {
 	public static void main(String args[]) {
 		ApplicationContext ioc = new ClassPathXmlApplicationContext("applicationcontext.xml");
 		Employee bean1 = ioc.getBean("emp", Employee.class);
-		Employee bean2 = ioc.getBean("emp2", Employee.class);
+//		Employee bean2 = ioc.getBean("emp2", Employee.class);
 		
 		System.out.println(bean1);
-		System.out.println(bean2);
+//		System.out.println(bean2);
 		
 		/*
 		 * bean1.getAddress().setCity("GZB"); bean1.getAddress().setState("UP");
